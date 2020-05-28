85a8105d3b2f37847f8e044e5521d2ea84bc5c84
Tony Sorensen <tony48853@gmail.com>
2020-05-27 18:29:29 -0700

    First two challenges done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function addNumbers(num1, num2) {
 // 👇 COMPLETE YOUR WORK BELOW 👇
 
 
-/**
+/*
  * ### Challenge `sayGoodbye`
  * 
  * @instructions
@@ -36,8 +36,8 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(name) {
+  return "Goodbye, " + name + ". Have a great day.";
 }
 
 /**
@@ -54,8 +54,9 @@ function sayGoodbye(/* code here */) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
-  /* code here */
+function temperatureCtoF(celsius) {
+  const fahrenheit = (celsius * 9)/5 + 32;
+  return Math.round(fahrenheit);
 }
 
 /**
