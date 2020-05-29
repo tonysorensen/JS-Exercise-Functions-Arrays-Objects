54ff053f4d10c433ef37cb4d283ee93d32b39662
Tony Sorensen <tony48853@gmail.com>
2020-05-28 20:09:30 -0700

    finished appleIndex

--- index.js
+++ index.js
@@ -144,9 +144,18 @@ function getName(objectName) {
  * passing in [ 'orange', 'grape', 'apple', 'banana', 'mango' ] as the argument,
  * the returned value should be: 2.
 */
-function appleIndex(/* code here */) {
-  /* code here */
-}
+function appleIndex(arr) {
+  for (i=0; i < arr.length; i++){
+    if (arr[i] === "apple"){
+      return [i]
+    }
+  }
+  
+  }
+  
+  
+   
+
 
 /**
  * ### Challenge `isItAnApple`
